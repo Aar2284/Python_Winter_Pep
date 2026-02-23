e19663129413b1fd322af0fcd6b5319ee28cc080
aaryan <aaryankalia165@gmail.com>
2026-02-23 16:24:51 +0530

    Basic Functions and Loops in JS, Dated :- 23/02

--- JS/first.js
+++ JS/first.js
@@ -55,3 +55,52 @@ while (j < 5) {
     console.log(j);
     j++;
 }
+
+// Arrow function
+
+const funtionName = (a,b) => {
+    return args;
+};
+
+// Example
+
+const getlength = (str) => {
+    return str.length;
+};
+
+// Arrow funtion to check if it is even or not
+
+const EvenNumber = (num) => {
+    if (num % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+};
+
+// arr.forEach (callback) method
+
+let numbers = [1,2,3,4,6,5,7];
+
+numbers.forEach((num) => {
+    console.log(num * num);
+});
+
+// map method
+
+let arr = [1,2,3,4];
+
+let newArr = arr.map((val) => {
+    return val * 2;
+});
+
+console.log(newArr);
+
+// shift and unshift method
+
+let arr11 = [2,3];
+arr11.unshift(1);
+console.log(arr11); 
+
+arr11.shift();
+console.log(arr11);
